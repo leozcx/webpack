5a7973967f7f9c13e60d8df25255f98e90cf9d5f
zhangchunxiao <zhangchunxiao@didichuxing.com>
2017-06-26 14:00:27 +0800

    adjust import order

--- template/src/main.js
+++ template/src/main.js
@@ -2,8 +2,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 {{/if_eq}}
-import App from '@/App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import * as dep from '@/import'
+import App from '@/App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 /* eslint-disable no-new */
 new dep.Vue({
